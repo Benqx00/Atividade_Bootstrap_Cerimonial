cd027da4a9f261f56bbceeb497ef21af2ba331e3
Benqx00 <thomazcandelaria58@gmail.com>
2026-09-09 10:39:12 -0300

    atividade

--- AtividadeBootstrap/Controllers/HomeController.cs
+++ AtividadeBootstrap/Controllers/HomeController.cs
@@ -1,24 +1,41 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using AtividadeBootstrap.Models;
+using EnlaceHomeMVC.Models;
 
-namespace AtividadeBootstrap.Controllers;
-
-public class HomeController : Controller
+namespace EnlaceHomeMVC.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            var model = new HomeViewModel
+            {
+                Titulo = "Cerimonias que contam sua historia",
+                Descricao = "Condução de cerimonias de casamento com acolhimento, elegancia e atenção a cada detalhe",
+                Botao = "Solicitar orçamento",
+                Servicos = new List<Servico>
+                {
+                    new Servico
+                    {
+                        Titulo = "Cerimonia Civil",
+                        Descricao = "Roteiro personalizado para o casal",
+                        Cor = "#cc2c08"
+                    },
+                    new Servico
+                    {
+                        Titulo = "Mestre de Cerimonia",
+                        Descricao = "Condução para o grande dia",
+                        Cor = "#8e0085"
+                    },
+                    new Servico
+                    {
+                        Titulo = "Acessoria de Roteiro",
+                        Descricao = "Ensaio e apoio no dia da cerimonia",
+                        Cor = "#e73bbd"
+                    }
+                }
+            };
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(model);
+        }
     }
 }
--- AtividadeBootstrap/obj/Debug/net10.0/AtividadeBootstrap.AssemblyInfo.cs
+++ AtividadeBootstrap/obj/Debug/net10.0/AtividadeBootstrap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadeBootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788f7a15948a741e82fe624ad077d9500e9d75eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e94730f6a0aed52a4b96101c7cfefacd0a5e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadeBootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadeBootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
